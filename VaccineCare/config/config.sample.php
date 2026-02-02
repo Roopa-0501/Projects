f767e1583bda282aabdecad8e439a17410f318be
Roopa-0501 <pavanroopa16@gmail.com>
2026-02-02 16:39:30 +0530

    Add files via upload

---
+++ VaccineCare/config/config.sample.php
@@ -0,0 +1,15 @@
+<?php
+return [
+  // Database
+  'DB_HOST' => 'localhost',
+   'DB_NAME' => 'vaccinecare',
+  'DB_USER' => 'xxxx',
+  'DB_PASS' => 'xxxxxx',
+
+
+  //Brevo (SMTP)
+  'BREVO_SMTP_USER' => 'xxxxxxxxxxx',
+  //'BREVO_SMTP_KEY'  => 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+];
+?>
